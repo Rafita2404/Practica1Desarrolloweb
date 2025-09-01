7f579e890f4f9c60388e3bb28185fdf4c2e8b74d
Rafita2404 <rafael.bravo63@unach.mx>
2025-08-31 22:06:24 -0600

    Elimina dist/ del control de versiones

--- dist/assets/index-BDkjuKOU.js
+++
@@ -1,1 +0,0 @@
-import{b1 as l,$ as o,b2 as a}from"./index-g9kfeV4j.js";function c(){l({variableName:o("scrollbar.width").name})}function b(){a({variableName:o("scrollbar.width").name})}export{c as b,b as u};
